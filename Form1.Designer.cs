2f5a82995dc7a0d2c0e7dc82c5304521826bb610
Abdessalam <48380521+abdessalam-aadel@users.noreply.github.com>
2025-03-14 15:40:49 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace MergeExcel
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.TxtBoxLoad = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.labelErrorMessage = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.Location = new System.Drawing.Point(537, 16);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(137, 29);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load ...";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // TxtBoxLoad
+            // 
+            this.TxtBoxLoad.Enabled = false;
+            this.TxtBoxLoad.Location = new System.Drawing.Point(21, 19);
+            this.TxtBoxLoad.Name = "TxtBoxLoad";
+            this.TxtBoxLoad.Size = new System.Drawing.Size(510, 22);
+            this.TxtBoxLoad.TabIndex = 1;
+            this.TxtBoxLoad.Text = "Chose your folder location ...";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.FlatAppearance.BorderSize = 2;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.Green;
+            this.btnStart.Location = new System.Drawing.Point(243, 73);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(163, 52);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Merge";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linkLabel1.Location = new System.Drawing.Point(304, 149);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(45, 16);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Github";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.ForeColor = System.Drawing.Color.Green;
+            this.labelInfo.Location = new System.Drawing.Point(26, 56);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(16, 16);
+            this.labelInfo.TabIndex = 4;
+            this.labelInfo.Text = "...";
+            // 
+            // labelErrorMessage
+            // 
+            this.labelErrorMessage.AutoSize = true;
+            this.labelErrorMessage.ForeColor = System.Drawing.Color.Red;
+            this.labelErrorMessage.Location = new System.Drawing.Point(23, 127);
+            this.labelErrorMessage.Name = "labelErrorMessage";
+            this.labelErrorMessage.Size = new System.Drawing.Size(0, 20);
+            this.labelErrorMessage.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MergeExcel.Properties.Resources.bureau;
+            this.pictureBox1.Location = new System.Drawing.Point(491, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(91, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 174);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelErrorMessage);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.TxtBoxLoad);
+            this.Controls.Add(this.btnLoad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Merge Multiple Excel";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox TxtBoxLoad;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label labelErrorMessage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+
+namespace MergeExcel
+{
+    public partial class frmMain : Form
+    {
+        // Array of Excel Files found in Folder
+        string[] XLSfiles;
+
+        // Store slected path of Folder browser dialog in variable
+        string selected_path;
+
+        // Create fileCount to counting number of Excel files found
+        int fileCount = 0;
+
+        Excel.Application excelApplication = null;
+        Excel.Workbook excelWorkBook = null;
+        object paramMissing = Type.Missing;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        // Handle Methode Search in all Sub-Directory and Get all Excel files found,
+        // and bring out to the string array
+        private int SearchXLSFiles(string path, out string[] XLSfiles)
+        {
+            XLSfiles = Directory
+                        .GetFiles(path, "*.*", SearchOption.AllDirectories)
+                        .Where(s => s.ToLower().EndsWith(".xls") || s.ToLower().EndsWith(".xlsx"))
+                        .ToArray();
+            return XLSfiles.Length;
+        }
+
+        // Methode Write exceptions into log file
+        static void LogException(string logFilePath, string filePath, Exception ex)
+        {
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                string filename = Path.GetFileNameWithoutExtension(filePath);
+                writer.WriteLine($"{filename} : {ex.Message}");
+            }
+        }
+
+        public void MergeExcelFiles(string[] filePaths, string destinationFile)
+        {
+            // Start Excel application
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = false; // Make Excel invisible during processing
+            // Disable display alerts to avoid the clipboard warning message
+            excelApp.DisplayAlerts = false;
+
+            // Create a new workbook to hold the merged data
+            Excel.Workbook destinationWorkbook = excelApp.Workbooks.Add();
+            Excel.Worksheet destinationWorksheet = destinationWorkbook.Sheets[1];
+
+            int currentRow = 1;
+            // Log file
+            string logFilePath = selected_path + @"\exceptions.log";
+            // Delete the log file if it exists
+            if (File.Exists(logFilePath))
+            {
+                File.Delete(logFilePath);
+            }
+
+            foreach (string filePath in filePaths)
+            {
+                try
+                {
+                    // Open the source workbook
+                    Excel.Workbook sourceWorkbook = excelApp.Workbooks.Open(filePath);
+
+                    foreach (Excel.Worksheet sourceWorksheet in sourceWorkbook.Sheets)
+                    {
+                        // Find the last row with data in the source worksheet
+                        Excel.Range sourceRange = sourceWorksheet.UsedRange;
+                        int lastRow = sourceRange.Rows.Count;
+
+                        // Copy the range from the source worksheet
+                        Excel.Range rangeToCopy = sourceWorksheet.Range["A1", sourceWorksheet.Cells[lastRow, sourceRange.Columns.Count]];
+                        rangeToCopy.Copy();
+
+                        // Paste the data into the destination worksheet
+                        destinationWorksheet.Cells[currentRow, 1].PasteSpecial(Excel.XlPasteType.xlPasteValues);
+
+                        // Update the current row position for the next sheet
+                        currentRow += lastRow;
+
+                        // Optionally, you can copy headers and add extra checks to ensure the right data is copied.
+                    }
+
+                    // Close the source workbook (no save)
+                    sourceWorkbook.Close(false);
+                }
+                catch (Exception ex)
+                {
+                    // Write Exception into exceptions.log
+                    LogException(logFilePath, filePath, ex);
+                    continue;
+                }
+                
+            }
+
+            // Save the destination workbook
+            destinationWorkbook.SaveAs(destinationFile);
+            destinationWorkbook.Close(false);
+
+            // Quit Excel application
+            excelApp.Quit();
+            Marshal.ReleaseComObject(excelApp);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (XLSfiles == null || string.IsNullOrEmpty(TxtBoxLoad.Text))
+            {
+                labelErrorMessage.Text = "No source folder was selected, Please select one.";
+                return;
+            }
+
+            else if (XLSfiles.Length == 0)
+            {
+                labelErrorMessage.Text = "No Excel file was found in the selected folder";
+                return;
+            }
+
+            labelErrorMessage.Text = "";
+            Cursor = Cursors.WaitCursor;
+            labelInfo.Text = "Processing ...";
+            labelErrorMessage.Text = "";
+
+            string destinationFile = @"C:\Users\P50\Desktop\mergedfile.xlsx";
+            try
+            {
+                MergeExcelFiles(XLSfiles, destinationFile);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            // Clear string array
+            XLSfiles = null;
+            Cursor = Cursors.Default;
+            TxtBoxLoad.Text = "Chose your folder location ...";
+            labelInfo.Text = "Done.";
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FD = new FolderBrowserDialog();
+            if (selected_path != null)
+                FD.SelectedPath = selected_path;
+            if (FD.ShowDialog() == DialogResult.OK)
+            {
+                string path = FD.SelectedPath;
+                selected_path = path;
+                TxtBoxLoad.Text = path;
+                fileCount = SearchXLSFiles(path, out XLSfiles);
+                labelInfo.Text = fileCount + " XLS files found.";
+            }
+        }
+
+        // Start Method CloseWorkBook
+        private void CloseWorkBook()
+        {
+            if (excelWorkBook != null)
+            {
+                // Close the workbook object.
+                excelWorkBook.Close(false, paramMissing, paramMissing);
+                excelWorkBook = null;
+            }
+        }
+
+        // Start Method QuitExcel
+        private void QuitExcel()
+        {
+            // Quit Excel and release the ApplicationClass object.
+            if (excelApplication != null)
+            {
+                excelApplication.Quit();
+                excelApplication = null;
+            }
+
+            // Force garbage collection.
+            GC.Collect();
+            // Wait for all finalizers to complete before continuing.
+            // Without this call to GC.WaitForPendingFinalizers,
+            // the worker loop below might execute at the same time
+            // as the finalizers. 
+            // With this call, the worker loop executes only after
+            // all finalizers have been called.
+            GC.WaitForPendingFinalizers();
+            // Clear string array
+            XLSfiles = null;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Go to Github repository
+            string url = "https://github.com/abdessalam-aadel/MergeExcel";
+
+            // Open the URL in the default web browser
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true // Ensures the URL is opened in the default web browser
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            // Condition >> Drag Folder
+            if (Directory.Exists(path))
+            {
+                TxtBoxLoad.Text = path;
+                fileCount = SearchXLSFiles(path, out XLSfiles);
+                selected_path = path;
+                // Check the Empty Folder
+                labelInfo.Text = fileCount == 0 ? "Your Folder is Empty." : fileCount + " XLS files found.";
+                labelErrorMessage.Text = "";
+            }
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+            TxtBoxLoad.Text = "Chose your folder location ...";
+            labelInfo.Text = "...";
+            labelErrorMessage.Text = "";
+            XLSfiles = null;
+        }
+    }
+}
